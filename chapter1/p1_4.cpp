6587336bcb86dea061dc30f757b1ecd2296c49ed
geoyee <geoyee@yeah.net>
2023-01-28 13:58:43 +0800

    [practice] 1.1-1.4

---
+++ chapter1/p1_4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string user_first_name;
+	string user_last_name;
+
+	cout << "Please enter your first name: ";
+	cin >> user_first_name;
+	cout << "Please enter your last name: ";
+	cin >> user_last_name;
+	cout << "\n"
+	     << "Hello, "
+	     << user_first_name
+	     << " "
+	     << user_last_name
+	     << " ... and goodbye!" << endl;
+
+	return 0;
+}
